dd30d727fdbd4c2f350e0563a0f826d245c0e494
Jonas Verch <jonas.verch@web.de>
2023-11-24 21:24:42 -0600

    Pseudo-Code complete

--- ABC_algorithm.R
+++ ABC_algorithm.R
@@ -19,7 +19,7 @@ calc_post_distr <- function(observed_data, tolerance_schedule, prior_distr, dist
   
   # Create dataframe of marginal model probabilities (length tolerance_schedule * number models)
   marginal_model_probs
-  # Create list of  population_dataframe for each epsilon (length tolerance_schedule) 
+  # Create list of population_dataframe for each epsilon (length tolerance_schedule) -> particle (model, parameter), weight
   list_populations  
 
   # Loop over tolerance_schedule (which define the populations)
@@ -44,7 +44,10 @@ calc_post_distr <- function(observed_data, tolerance_schedule, prior_distr, dist
     }
     list_popluations[[t]] <- population_dataframe
   }
-  
+  Normalized_weights <- normalize_weights(list_populations[[t]]$weights)
+  list_populations[[t]] # add normalized_weights
+  marginal_model_probs[t] <- calc_marginal_model_probs(list_populations[[t]]) 
+
 }
 
 
@@ -65,11 +68,11 @@ sim_i_data <- function(list_populations,
       sample_model_param <- prior_distr()
     }
     else {
-      # random draw from population t-1
-      # model <- KM()
-      # sample parameter with weights from previous population 
-      # parameter <- PM()
-      sample_model_param 
+      # m_star <- random draw from population t-1 with weights of previous population
+      # model <- KM(m_star)
+      # param_star <- sample parameter with weights from previous population restricted on model drawn (model)
+      # parameter <- PM(param_star) 
+      sample_model_param # complete c(parameter, model) 
     }
     
     prior_prob_sample <- prior_distr(sample_model_param)
@@ -80,22 +83,23 @@ sim_i_data <- function(list_populations,
     if (distance_fct(candidate_data, observed_data) > epsilon) next
     
     weight_particle <- calc_particle_weight(candidate_data, 
+                                   sample_model_param,
                                    population_index, 
                                    marginal_model_probs, 
                                    perturb_kernels,
                                    prior_prob_sample) #calculate weight of particle if accepted
-    return(weight_particle)
+    return(sample_model_param, weight_particle)
   }
 }
   
-calc_particle_weight <- function(candidate_data, population_index, marginal_model_probs, perturb_kernels, prior_prob_sample) {
+calc_particle_weight <- function(candidate_data, sample_model_param, population_index, marginal_model_probs, perturb_kernels, prior_prob_sample) {
   if (population_index == 1) {
     return(1) #!# Must include calculation of bt (didn't understand it -> number of replicae simulation runs)
   }
   else {
     bt <- 1 #!# Must include calculation of bt (didn't understand it -> number of replicae simulation runs)
-    S <- x# Sum over all possible models (i):
-          # marginal_model_probs[population_index - 1, i] * perturb_kernels[[1]]() * SUM over parameter associated with sampled model in last population ((weight of parameter in last pop * KP) / prob of model in last population) #!# didn't understand properly
+    S <- # Sum over all possible models (j):
+          # marginal_model_probs[population_index - 1, j] * perturb_kernels[[1]](sample_model_param(model, model j)) * SUM over parameter associated with sampled model in last population ((weight of parameter in last pop * KP) / prob of model in last population) #!# didn't understand properly
     weight_particle <- x# (prior_prob_sample * bt) / S
     return(weight_particle)
   }
@@ -103,4 +107,11 @@ calc_particle_weight <- function(candidate_data, population_index, marginal_mode
   
 }
 
+normalize_weights <- function(weights) {
+  # normalize as weights / sum(weights)
+}
 
+calc_marginal_model_probs <- function(population) {
+  # sum weights for each model / sum of weights
+  # return marginal model probabilities
+}
