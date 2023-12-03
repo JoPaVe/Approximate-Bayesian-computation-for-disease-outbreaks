b52e337d0e4de2186e4b7d9d741193ad836f89f7
hanush14 <hanush.anand@gmail.com>
2023-12-03 17:05:25 -0500

    Update from main

--- ABC_algorithm.R
+++ ABC_algorithm.R
@@ -1,67 +1,90 @@
 
-calc_post_distr_base <- function(observed_data, epsilon, prior_distr, model_number_params, distance_fct, data_generating_fct, N_particles) {
-  ## Function calculates the posterior distribution of model and parameter
-  #
-  # observed_data: Observed data
-  # prior_distr: list -> 1: function of prior distribution for models #!# -> could be relaxed to selection of prior
-  #                      2: list of functions of prior distributions for parameter of each model (length is model number) -> first entry is number of parameters
-  # model_number_params: vector or model parameter: c(modelparam1, modelparam2, ...)
-  # distance_fct: function distance function (candidate_data, observed_data)
-  # N_particles: Integer of N particle samples
-  # data_generating_fct: Data generating function
-  # tolerance: Integer defining epsilon.
-  ## 
+CalculatePosteriorBase <- function(observed_data, kEpsilon, prior_distr, model_number_params, DistanceFct, DataGeneratingFct, kNparticles) {
+  
+  ## Function calculates the posterior distribution of parameter of a previously specified model and marginal model probabilities
+  ## Implementation according to Toni T., Stumpf M.P.H. (2009) - Simulation-based model selection for dynamical systems in systems and population biology
+  
+  # Args:
+  #   observed_data:       Observed data
+  #   kEpsilon:           tolerance level that determines how close the simulated data should be to the observed data in integer
+  #   prior_distr:         List -> 1: Function for prior distribution of models
+  #                                2: List -> 1: Function of prior distributions of parameter for first model 
+  #                                           2: Function of prior distributions of parameter for second model
+  #                                           ... 
+  #   model_number_params: Vector -> Number of parameters for each model
+  #   DistanceFct:         Function that calculates the distance between simulated and observed data
+  #   kNparticles:         Integer of Number of particle samples to draw
+  #   DataGeneratingFct:   Function that generates data based on model parameters
+  
+  # Returns:
+  #   List -> 1: marginal_model_probs: Data frame of marginal probabilities for each model
+  #           2: List posterior_model_distributions -> 1: Data frame of accepted parameter for first model
+  #                                                    2: Data frame of accepted parameter for second model
+  #                                                    ...
+  #           3: Ratio of accepted to attempted data samples
+  # 
   
   
-  # Test that all inputs are properly defined 
+  ## Initialization
   
-  
-  
+  # Number of models as number of different parameter priors
   number_models <- length(prior_distr[[2]])
+  
+  # Number of parameter
   number_params <- sum(model_number_params)
   
-  
-  
-  # Create dataframe of marginal model probabilities (length tolerance_schedule * number models)
+  # Data frame of marginal model probabilities -> nrows: 1, ncols: number models
   marginal_model_probs <- data.frame(matrix(NA, nrow = 1, ncol = number_models))
-  # Create dataframe of accepted particles
-  # list of data.frame with accepted particles for every model
+  
+  # List of Data Frame of accepted parameter for every model -> nrows: 1, ncol: number of model parameter for every model
   posterior_model_distributions <- list()
-  for (model in 1:number_models) {
-    posterior_model_distributions[[model]] <- data.frame(matrix(NA, nrow = 1, ncol = model_number_params[model]))
+  for (model_number in 1:number_models) {
+    posterior_model_distributions[[model_number]] <- data.frame(matrix(NA, nrow = 1, ncol = model_number_params[model_number]))
   }
   
+  # Number of attempted and accepted data samples
   accepted <- 1
   attempted <- 1
-  # Loop until accepted == N_particles 
-  while(accepted <= N_particles) {
+  
+  
+  ## Loop until kNparticles of parameters are accepted
+  
+  while(accepted <= kNparticles) {
     
     # Draw m*
     model_draw <- prior_distr[[1]]()
     
     # Draw theta*
-    param_draw <- prior_distr[[2]][[model_draw]]()
+    param_draw <- prior_distr[[2]][[model_draw]]()  # Draw theta* from model parameter
     
-    # Create candidate dataset
-    sample_data <- data_generating_fct(param_draw, observed_data)
+    # Generate candidate dataset
+    sample_data <- DataGeneratingFct(param_draw, observed_data)
     
-    # compute distance
-    distance_data <- distance_fct(observed_data, sample_data)
+    # Compute distance
+    distance_data <- DistanceFct(observed_data, sample_data)
     
     
-    # Store or next if distance > epsilon
-    if (distance_data <= epsilon) {
-      posterior_model_distributions[[model_draw]][accepted,] <- param_draw
+    # Store if distance <= epsilon, next otherwise
+    if (distance_data <= kEpsilon) {
+      posterior_model_distributions[[model_draw]][accepted,] <- param_draw  # Store theta* for model m*
       accepted <- accepted + 1
+      print(accepted)
     } 
     attempted <- attempted + 1
   }
   
+  # Remove all NA rows
+  posterior_model_distributions <- lapply(posterior_model_distributions, FUN = function(param_df) {
+    return(param_df |> na.exclude()) 
+  })
+  
   # Compute marginal model probabilities
   marginal_model_probs <- sapply(1:number_models, FUN = function(model) {
-    model_marginale_probability <- dim(posterior_model_distributions[[model]])[1] / N_particles
+    model_marginale_probability <- dim(posterior_model_distributions[[model]])[1] / kNparticles  # Number of accepted parameters for model m' / number of total accepted particles 
     return(model_marginale_probability)
   })
   
-  return(list(marginal_model_probs, posterior_model_distributions, accepted / attempted))
+  return(list("marginal_model_probabilities" = marginal_model_probs, 
+              "accepted_parameter" = posterior_model_distributions, 
+              "ratio_accepted_parameter" = accepted / attempted))
 }
